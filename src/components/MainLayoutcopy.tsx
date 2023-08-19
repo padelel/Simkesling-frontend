d3eb4796a9b9d8b49bbfa8a9ad6360cf7c6ce4d6
davirama <59201121+davirama@users.noreply.github.com>
2023-08-19 16:08:47 +0700

    coba navigation part1

--- src/components/MainLayoutcopy.tsx
+++ src/components/MainLayoutcopy.tsx
@@ -9,11 +9,29 @@ import {
   ProfileOutlined,
   LogoutOutlined,
 } from "@ant-design/icons";
-import { Layout, Menu, theme } from "antd";
+import { Layout, Menu, MenuProps, theme } from "antd";
 import React, { ReactNode, useState } from "react";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import Link from "next/link";
+
+type MenuItem = Required<MenuProps>["items"][number];
+
+function getItem(
+  label: React.ReactNode,
+  key: React.Key,
+  icon?: React.ReactNode,
+  children?: MenuItem[],
+  type?: "group"
+): MenuItem {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+  } as MenuItem;
+}
 
 interface MainLayoutProps {
   children: ReactNode;
@@ -28,61 +46,82 @@ const MainLayout: React.FC<MainLayoutProps> = ({ children, title }) => {
   } = theme.useToken();
   const [collapsed, setCollapsed] = useState(false);
 
-  const items = [
-    {
-      icon: <DashboardOutlined />,
-      label: "Dashboard",
-      url: "/dashboardpuskesmas",
-    },
-    {
-      icon: <CarOutlined />,
-      label: "Pengajuan Transporter",
-      url: "/dashboardpuskesmas/pengajuantransporter",
-    },
-    {
-      icon: <OrderedListOutlined />,
-      label: "List Transporter",
-      url: "/dashboardpuskesmas/transporter",
-    },
-    {
-      icon: <BarChartOutlined />,
-      label: "Laporan Limbah",
-      url: "/dashboardpuskesmas/limbah",
-    },
-    {
-      icon: <ProfileOutlined />,
-      label: "Profil Saya",
-      url: "/dashboardpuskesmas/profilepuskesmas",
-    },
-    {
-      icon: <LogoutOutlined />,
-      label: "Logout",
-    },
-  ].map((item, index) => ({
-    key: (index + 1).toString(),
-    icon: item.icon,
-    label: item.label,
-    url: item.url,
-  }));
+  const items: MenuProps["items"] = [
+    getItem(
+      "Group",
+      "grp",
+      null,
+      [
+        getItem("Dashboard", "/dashboardpuskesmas"),
+        getItem(
+          "Pengajuan Transporter",
+          "/dashboardpuskesmas/pengajuantransporter"
+        ),
+      ],
+      "group"
+    ),
+  ];
+
+  // const items = [
+  //   {
+  //     icon: <DashboardOutlined />,
+  //     label: "Dashboard",
+  //     url: "/dashboardpuskesmas",
+  //   },
+  //   {
+  //     icon: <CarOutlined />,
+  //     label: "Pengajuan Transporter",
+  //     url: "/dashboardpuskesmas/pengajuantransporter",
+  //   },
+  //   {
+  //     icon: <OrderedListOutlined />,
+  //     label: "List Transporter",
+  //     url: "/dashboardpuskesmas/transporter",
+  //   },
+  //   {
+  //     icon: <BarChartOutlined />,
+  //     label: "Laporan Limbah",
+  //     url: "/dashboardpuskesmas/limbah",
+  //   },
+  //   {
+  //     icon: <ProfileOutlined />,
+  //     label: "Profil Saya",
+  //     url: "/dashboardpuskesmas/profilepuskesmas",
+  //   },
+  //   {
+  //     icon: <LogoutOutlined />,
+  //     label: "Logout",
+  //   },
+  // ].map((item, index) => ({
+  //   key: (index + 1).toString(),
+  //   icon: item.icon,
+  //   label: item.label,
+  //   url: item.url,
+  // }));
 
   const router = useRouter();
   const [selectedKey, setSelectedKey] = useState("1"); // Default selected key
+  const onClick: MenuProps["onClick"] = (e) => {
+    console.log("click ", e);
+    setSelectedKey(e.key);
+    router.push(e.key);
+  };
 
-  const handleMenuItemClick = (url: any, key: any) => {
-    console.debug("handleMenuItemClick:", key, url);
-    if (url) {
-      setSelectedKey(key);
-      router.push(url);
-    }
-  };
-  const handleMenuClick = (isi: any) => {
-    console.debug(isi);
-    // console.debug("handleMenuItemClick:", key, url);
-    // if (url) {
-    //   setSelectedKey(key);
-    //   router.push(url);
-    // }
-  };
+  // const handleMenuItemClick = (url: any, key: any) => {
+  //   console.debug("handleMenuItemClick:", key, url);
+  //   if (url) {
+  //     setSelectedKey(key);
+  //     router.push(url);
+  //   }
+  // };
+  // const handleMenuClick = (isi: any) => {
+  //   console.debug(isi);
+  //   console.debug("handleMenuItemClick:", key, url);
+  //   if (url) {
+  //     setSelectedKey(key);
+  //     router.push(url);
+  //   }
+  // };
 
   return (
     <Layout style={{ minHeight: "100vh" }}>
@@ -142,19 +181,26 @@ const MainLayout: React.FC<MainLayoutProps> = ({ children, title }) => {
             Puskesmas Pasir Gunung Selatan
           </h5>
         </div>
-        <Menu mode="inline" onClick={handleMenuClick}>
+        {/* <Menu mode="inline" onClick={handleMenuClick}>
           {items.map((item, index) => {
             console.log(item);
             return (
               <Menu.Item
                 key={item.key}
                 icon={item.icon}
+                to
                 onClick={() => handleMenuItemClick(item.url, item.key)}>
-                <Link href={item.url ?? ""}>{item.label}</Link>
+                <div>{item.label}</div>
               </Menu.Item>
             );
           })}
-        </Menu>
+        </Menu> */}
+        <Menu
+          onClick={onClick}
+          defaultSelectedKeys={[selectedKey]}
+          mode="inline"
+          items={items}
+        />
       </Sider>
       <Layout>
         <Header style={{ padding: 0, background: colorBgContainer }}>
