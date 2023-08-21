ae23e188e0bcbd661235e11b0d17ac8699afcd94
AIRGG <>
2023-08-22 01:24:44 +0700

    coba ganti base_url

--- src/pages/utils/HttpRequest.tsx
+++ src/pages/utils/HttpRequest.tsx
@@ -2,7 +2,8 @@ import axios from "axios";
 // Create an instance of Axios with custom configurations
 const api = axios.create({
   // baseURL: 'http://localhost:8000/api',
-  baseURL: "http://localhost:8000/api/v1", // Your API base URL
+  // baseURL: "http://localhost:8000/api/v1", // Your API base URL
+  baseURL: "http://be-simkesling.lalapan-depok.com/api/v1", // Your API base URL
   timeout: 30000, // Request timeout in milliseconds
   headers: {
     // "Content-Type": "application/json",
